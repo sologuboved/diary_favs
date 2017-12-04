465a3727f2ff1242f8c57bb98b8b416a52b92254
sologuboved <sologuboved@gmail.com>
2017-12-04 22:47:51 +0300

    added select_threads

---
+++ select_threads.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+fanfiction = sorted(list({203269624, 204272732, 204724779, 205473744, 205717090, 207019021, 207160697, 207229626,
+                          207475425, 208437011, 209357016, 209458474, 210078603, 210118502, 210976566, 211057726,
+                          211224373, 211454454, 211486742, 211611872, 211747040, 211855215, 211855240, 212020164,
+                          212106345, 212174022, 212364124, 212437556, 213317402, 213317474, 213317521, 213398325,
+                          213418366, 213468138, 213542873, 213595204, 213595231, 213624620, 213657323, 213716107,
+                          213743031, 213830008, 213945293}))
+
+social = sorted(list({194747534, 209893456, 209999261, 210078711, 210118423, 210689838, 210735617, 211084466,
+                      211153445, 211224485, 211258645, 211810591, 212173939, 212973780, 213089863, 213317426,
+                      213468122, 213808430}))
+
+misc = [214162980]
+
+
+creepy = 'http://fandomnaya-pravda.diary.ru/?tag=127627'
+roleplay = 'http://fandomnaya-pravda.diary.ru/?tag=2515'
+femslash = 'http://fandomnaya-pravda.diary.ru/?tag=12113'
+perev1 = 'http://fandomnaya-pravda.diary.ru/?tag=1738138'
+perev2 = 'http://pravdoruboklon.diary.ru/?tag=5491638'
+rsya1 = 'http://fandomnaya-pravda.diary.ru/?tag=1689730'
+rsya2 = 'http://pravdoruboklon.diary.ru/?tag=5475332'
+o_e = 'http://fandomnaya-pravda.diary.ru/?tag=25975'
+socionics = 'http://fandomnaya-pravda.diary.ru/?tag=1083'
+kinks = 'http://fandomnaya-pravda.diary.ru/?tag=20239'
