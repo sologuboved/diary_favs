ffefdb7fed644c7362a3e04670239a45e5ded136
sologuboved <sologuboved@gmail.com>
2017-12-09 23:03:35 +0300

    added bulk_rename and global_vars

---
+++ bulk_rename.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# C:/Users/vesna/Documents/Werecoder/diary_favs_py3/o_e_threads2/thread1.txt
+
+import os
+
+
+def rename_files():
+    for foldername in os.listdir('.'):
+        if '.' not in foldername and '__' not in foldername and '_old' in foldername:
+            print()
+            print(foldername)
+            for old_filename in os.listdir(foldername):
+                filename, extension = old_filename.split('.')
+                new_filename = filename + '_old.' + extension
+                os.rename(foldername + '/' + old_filename, foldername + '/' + new_filename)
+
+
+def rename_directories():
+    for old_foldername in os.listdir('.'):
+        if '.' not in old_foldername and '__' not in old_foldername and '2' in old_foldername:
+            new_foldername = old_foldername[: -1] + '_old'
+            os.rename(old_foldername, new_foldername)
+
+
+if __name__ == '__main__':
+    rename_files()
+
---
+++ global_vars.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+THREAD = 'thread'
+URL = 'http://%s.diary.ru/p%s.htm'
+F_P = 'fandomnaya-pravda'
+P_C = 'pravdoruboklon'
+FROM = '&from='
+NO_POST = 'no post'
+
+FOLDER_POSTFIX = '_threads/'
+CREEPY = 'creepy'
+FANFICTION = 'fanfiction'
+SOCIAL = 'social'
+ROLEPLAY = 'roleplay'
+RSYA = 'rsya'
+FEMSLASH = 'femslash'
+O_E = 'o_e'
+SOCIONICS = 'socionics'
+KINKS = 'kinks'
+MISC = 'misc'
